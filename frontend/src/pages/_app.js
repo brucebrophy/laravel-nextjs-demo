f8f47598c903f47304ea3ea6e79e4ad7e5adca30
Bruce Brophy <bruce@brucebrophy.com>
2023-01-13 16:31:39 -0500

    Adds Apollo Client

--- frontend/src/pages/_app.js
+++ frontend/src/pages/_app.js
@@ -1,5 +1,13 @@
+import { ApolloProvider } from '@apollo/client'
 import 'tailwindcss/tailwind.css'
+import client from '../apollo-client'
 
-const App = ({ Component, pageProps }) => <Component {...pageProps} />
+const App = ({ Component, pageProps }) => {
+    return (
+        <ApolloProvider client={client}>
+            <Component {...pageProps} />
+        </ApolloProvider>
+    )
+}
 
 export default App
